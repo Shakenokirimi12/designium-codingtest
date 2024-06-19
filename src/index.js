d0073d000823617fd0ff86dba5f956e86f0ec3ba
shakenokirimi12 <ken20051205@gmail.com>
2024-06-19 15:05:18 +0900

    誤った切り分けを削除

--- src/index.js
+++ src/index.js
@@ -13,32 +13,40 @@ export default {
 					let userName = data.name;
 					let userEmail = data.email;
 					let userPassword = data.password;
+					const id = btoa(userEmail + userName);//generate id roughly for now
+					try {
+						const { results: queryresult } = await env.DATABASE.prepare(
+							"SELECT * FROM Users WHERE userId = ?"
+						).bind(id).all();
+						if (queryresult.length != 0) {
+							return new Response(JSON.stringify({ "error": "multiple account", "message": "You have multiple account on server. Please contact support." }), await headerBuilder(500))
+						}
+						else if (queryresult.length == 0) {
+							await env.DATABASE.prepare(
+								"INSERT INTO Users VALUES(?,?,?,?)"
+							).bind(id, userName, userEmail, userPassword)
+								.run();
+							return new Response(JSON.stringify({
+								"id": id,
+								"name": userName,
+								"email": userEmail
+							}), await headerBuilder(201));
+						}
+					}
+					catch (error) {
+						return new Response(JSON.stringify({ "error": "Database error.", "message": error }), await headerBuilder(500))
+					}
 				}
 				catch (error) { //when failed to get parameters in request json
 					console.log(error);
-					return new Response({ "error": "required value not specified.", "message": "You don't specify some or any parameters that you have to post." }, await headerBuilder(400))
+					return new Response(JSON.stringify({ "error": "required value not specified.", "message": "You don't specify some or any parameters that you have to post." }), await headerBuilder(400))
 				}
-				const id = btoa(userEmail + userName);//generate id roughly for now
-				try {
-					await env.DATABASE.prepare(
-						"INSERT INTO Users VALUES(?,?,?,?)"
-					).bind(id, userName, userEmail, userPassword)
-						.run();
-				}
-				catch (error) {
-					return new Response({ "error": "Database error.", "message": error }, await headerBuilder(500))
-				}
-				return new Response({
-					"id": id,
-					"name": userName,
-					"email": userEmail
-				}, await headerBuilder(201));
 			}
 			else {//When method for pathname "/user" is NOT "POST"
-				return new Response({
+				return new Response(JSON.stringify({
 					"error": "Method not allowed.",
-					"message": "Your method is not allowed. Method must be POST, GET, PUT, or DELETE."
-				}, await headerBuilder(405));
+					"message": "Your method is not allowed."
+				}), await headerBuilder(405));
 			}
 		}
 		else if (pathname === "/login") {
@@ -47,105 +55,137 @@ export default {
 					const data = await request.json();
 					let userEmail = data.email;
 					let userPassword = data.password;
+					//何らかの処理
+					if (true) {
+						const token = btoa(userEmail);//generate token for now roughly
+						return new Response(JSON.stringify({
+							"token": token
+						}), await headerBuilder(200));
+					}
+					else {
+						return new Response(JSON.stringify({ "error": "Authentication failed.", "message": "You can not log in to service. Maybe you entered wrong password or email?" }), await headerBuilder(401));
+					}
 				}
 				catch (error) {
 					console.log(error);
-					return new Response({ "error": "required value not specified.", "message": "You don't specify some or any parameters that you have to post." }, await headerBuilder(400))
-				}
-				//何らかの処理
-				if (true) {
-					const token = btoa(userEmail);//generate token for now roughly
-					return new Response({
-						"token": token
-					}, await headerBuilder(200));
-				}
-				else {
-					return new Response({ "error": "Authentication failed.", "message": "You can not log in to service. Maybe you entered wrong password or email?" }, await headerBuilder(401));
+					return new Response(JSON.stringify({ "error": "required value not specified.", "message": "You don't specify some or any parameters that you have to post." }), await headerBuilder(400))
 				}
 			}
 			else {
-				return new Response({
+				return new Response(JSON.stringify({
 					"error": "Method not allowed.",
-					"message": "Your method is not allowed. Method must be POST, GET, PUT, or DELETE."
-				}, await headerBuilder(405));
+					"message": "Your method is not allowed."
+				}), await headerBuilder(405));
 			}
 		}
-		else if (pathname.match(/^\/users\/(\d+)$/)) {
+		else if (pathname.match(/^\/users\/[a-zA-Z0-9]+$/)) {
 			if (request.method == "GET") {
-				const id = pathname.match(/^\/users\/(\d+)$/)[1];
+				const match = pathname.match(/^\/users\/([a-zA-Z0-9]+)$/);
+				const id = match[1];
+				console.log(id)
 				try {
 					const { results: queryresult } = await env.DATABASE.prepare(
 						"SELECT * FROM Users WHERE userId = ?"
 					).bind(id).all();
+					if (queryresult.length != 1 && queryresult.length != 0) {
+						return new Response(JSON.stringify({ "error": "Error: multiple account", "message": "You have multiple account on server. Please contact support." }), await headerBuilder(500))
+					}
+					else if (queryresult.length == 0) {
+						return new Response(JSON.stringify({ "error": "User not found.", "message": "Specified user not found in this server..." }), await headerBuilder(404))
+					}
+					else {
+						const userId = queryresult[0].id;
+						const userName = queryresult[0].userName;
+						const userEmail = queryresult[0].userEmail
+						return new Response(JSON.stringify({
+							"id": userId,
+							"name": userName,
+							"email": userEmail
+						}), await headerBuilder(200))
+					}
 				}
 				catch (error) {
-					return new Response({ "error": "Database error.", "message": error }, await headerBuilder(500))
+					console.log(error);
+					return new Response(JSON.stringify({ "error": "Database error.", "message": error }), await headerBuilder(500))
 				}
-				if (queryresult.length != 1 && queryresult.length != 0) {
-					return new Response({ "error": "Error: multiple account", "message": "You have multiple account on server. Please contact support." }, await headerBuilder(500))
-				}
-				else if (queryresult.length == 0) {
-					return new Response({ "error": "User not found.", "message": "Specified user not found in this server..." }, await headerBuilder(404))
-				}
-				else {
-					const userId = queryresult[0].id;
-					const userName = queryresult[0].userName;
-					const userEmail = queryresult[0].userEmail
-					return new Response({
-						"id": userId,
-						"name": userName,
-						"email": userEmail
-					}, await headerBuilder(404))
-				}
-			}
-			else if (request.method == "PUT") {
 
 			}
 			else if (request.method == "DELETE") {
-				const id = pathname.match(/^\/users\/(\d+)$/)[1];
+				const match = pathname.match(/^\/users\/([a-zA-Z0-9]+)$/);
+				const id = match[1];
+				try {
+					const { results: queryresult } = await env.DATABASE.prepare(
+						"SELECT * FROM Users WHERE userId = ?"
+					).bind(id).all();
+					if (queryresult.length == 0) {
+						return new Response(JSON.stringify({ "error": "User not found.", "message": "Specified user not found in this server..." }), await headerBuilder(404))
+					}
+					else {
+						try {
+							await env.DATABASE.prepare(
+								"DELETE FROM Users WHERE userId = ? "
+							).bind(id).all();
+							return new Response(JSON.stringify({ "message": "Sucesfully deleted user." }), await headerBuilder(200))
+						}
+						catch (error) {
+							console.log(error)
+							return new Response(JSON.stringify({ "error": "Unauthorized", "message": error }), await headerBuilder(403))
+						}
+					}
+				}
+				catch (error) {
+					console.log(error)
+					return new Response(JSON.stringify({ "error": "Database error.", "message": error }), await headerBuilder(500))
+				}
+			}
+			else if (request.method == "PUT") {
+				const match = pathname.match(/^\/users\/([a-zA-Z0-9]+)$/);
+				const id = match[1];
 				try {
 					const data = await request.json();
 					let userEmail = data.email;
 					let userName = data.name;
+					if (userEmail == undefined || userName == undefined) {
+						return new Response(JSON.stringify({ "error": "required value not specified.", "message": "You don't specify some or any parameters that you have to post." }), await headerBuilder(400))
+					}
+					try {
+						const { results: queryresult } = await env.DATABASE.prepare(
+							"SELECT * FROM Users WHERE userId = ?"
+						).bind(id).all();
+
+						if (queryresult.length == 0) {
+							return new Response(JSON.stringify({ "error": "User not found.", "message": "Specified user not found in this server..." }), await headerBuilder(404))
+						}
+						else {
+							try {
+								await env.DATABASE.prepare(
+									"UPDATE Users SET userName = ? ,userEmail = ? WHERE userId = ? "
+								).bind(userName, userEmail, id).all();
+								return new Response(JSON.stringify({ "message": "Updated successfully." }), await headerBuilder(200))
+							}
+							catch (error) {
+								console.log(error);
+								return new Response(JSON.stringify({ "error": "Unauthorized", "message": error }), await headerBuilder(403))
+							}
+						}
+
+					}
+					catch (error) {
+						console.log(error)
+						return new Response(JSON.stringify({ "error": "Database error.", "message": error }), await headerBuilder(500))
+					}
 				}
 				catch (error) {
 					console.log(error);
-					return new Response({ "error": "required value not specified.", "message": "You don't specify some or any parameters that you have to post." }, await headerBuilder(400))
+					return new Response(JSON.stringify({ "error": "required value not specified.", "message": "You don't specify some or any parameters that you have to post." }), await headerBuilder(400))
 				}
-				try {
-					const { results: queryresult } = await env.DATABASE.prepare(
-						"SELECT * FROM Users WHERE userId = ?"
-					).bind(id).all();
-				}
-				catch (error) {
-					return new Response({ "error": "Database error.", "message": error }, await headerBuilder(500))
-				}
-				if (queryresult.length == 0) {
-					return new Response({ "error": "User not found.", "message": "Specified user not found in this server..." }, await headerBuilder(404))
-				}
-
-				try {
-					await env.DATABASE.prepare(
-						"UPDATE * Users SET userName = ? AND userEmail = ? WHERE userId = ? "
-					).bind(userName, userEmail, id).all();
-
-				}
-				catch (error) {
-					return new Response({ "error": "Unauthorized", "message": error }, await headerBuilder(403))
-				}
-				return new Response("", await headerBuilder(204))
 			}
 			else {
-				return new Response({ "error": "Method is wrong.", "message": "Specified method cannot be used in this server..." }, await headerBuilder(405))
+				return new Response(JSON.stringify({ "error": "Method is wrong.", "message": "Specified method cannot be used in this server..." }), await headerBuilder(405))
 			}
 		}
 		else {
-			if (request.method == "GET") {
-				return new Response(JSON.stringify({ "error": "Pathname is wrong.", "message": "Specified pathname not found in this server..." }), await headerBuilder(404))
-			}
-			else {
-				return new Response({ "error": "Pathname is wrong.", "message": "Specified pathname not found in this server..." }, await headerBuilder(404))
-			}
+			return new Response(JSON.stringify({ "error": "Pathname is wrong.", "message": "Specified pathname not found in this server..." }), await headerBuilder(404))
 		}
 	},
 };
